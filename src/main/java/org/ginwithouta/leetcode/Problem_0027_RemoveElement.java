ea92c8ea148d459e21246bbc2dcb9f3f0a76007f
ginwithouta <ginwithouta@gmail.com>
2023-12-04 12:18:26 +0800

    【FEATURE】解决 LeetCode 0027 题，原地移除数组中值为给定值的元素，返回新数组的长度

---
+++ src/main/java/org/ginwithouta/leetcode/Problem_0027_RemoveElement.java
@@ -0,0 +1,23 @@
+package org.ginwithouta.leetcode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2023/12/4
+ * Problem 原地移除数组中值为给定值的数字
+ */
+public class Problem_0027_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        /*
+         * 只要碰到相同的数字，就将最后一个元素放到当前位置，然后再次判断，直到遍历完一整个数组
+         */
+        int length = nums.length, cur = 0;
+        while (cur < length) {
+            if (nums[cur] == val) {
+                nums[cur] = nums[--length];
+            } else {
+                ++cur;
+            }
+        }
+        return length;
+    }
+}
