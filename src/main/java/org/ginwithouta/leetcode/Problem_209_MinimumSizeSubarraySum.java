bd1f7cead5d21dfe9270f1d73e0298d8b4ac5d33
ginwithouta <ginwithouta@gmail.com>
2023-12-14 15:49:10 +0800

    【FEATURE】解决 LeetCode 0209 题，长度最小的子数组

---
+++ src/main/java/org/ginwithouta/leetcode/Problem_209_MinimumSizeSubarraySum.java
@@ -0,0 +1,30 @@
+package org.ginwithouta.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2023/12/13
+ */
+public class Problem_209_MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        /*
+         * 滑动窗口经典问题，只要不断往后移动窗口就行，小于 target 就移动右窗口，大于等于 target 就移动左窗口
+         */
+        if (nums.length == 1) {
+            return nums[0] >= target ? 1 : 0;
+        }
+        int len = 0, sum = nums[0], left = 0, right = 0;
+        while (true) {
+            if (sum >= target) {
+                len = len == 0 ? right - left + 1 : Math.min(len, right - left + 1);
+                sum -= nums[left++];
+            } else {
+                if (right == nums.length - 1) {
+                    return len;
+                }
+                sum += nums[++right];
+            }
+        }
+    }
+}
