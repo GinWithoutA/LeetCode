ef642ec466f3b01a74e4ec68239528e85a89244d
ginwithouta <ginwithouta@gmail.com>
2023-12-27 15:48:16 +0800

    【FEATURE】解决 LeetCode 0242 题，有效的字母异位词

--- src/main/java/org/ginwithouta/leetcode/Problem_0242_ValidAnagram.java
+++ src/main/java/org/ginwithouta/leetcode/Problem_0242_ValidAnagram.java
@@ -4,6 +4,7 @@ package org.ginwithouta.leetcode;
  * @author Ginwithouta
  * Generate at 2023/12/27
  * Problem 有效的字母异位词
+ * @link Problem_0049_GroupAnagrams
  */
 public class Problem_0242_ValidAnagram {
     public boolean isAnagram(String s, String t) {
