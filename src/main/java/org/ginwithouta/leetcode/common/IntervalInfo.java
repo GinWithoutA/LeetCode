8b1dd7c970aa905d9cb5cd4c450c53b4cb0c60aa
ginwithouta <ginwithouta@gmail.com>
2024-01-05 13:01:23 +0800

    【FEATURE】解决 LeetCode 0452 题，用最少数量的箭引爆气球

---
+++ src/main/java/org/ginwithouta/leetcode/common/IntervalInfo.java
@@ -0,0 +1,15 @@
+package org.ginwithouta.leetcode.common;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/1/5
+ * 区间信息类
+ */
+public class IntervalInfo {
+    public int start;
+    public int end;
+    public IntervalInfo(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
