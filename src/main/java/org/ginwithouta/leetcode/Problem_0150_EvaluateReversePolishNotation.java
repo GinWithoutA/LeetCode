9464953adc6c92812747d8f6f9ec44a1153388fa
ginwithouta <ginwithouta@gmail.com>
2024-01-21 23:05:22 +0800

    【FEATURE】解决 LeetCode 0150 题，逆波兰表达式求值

---
+++ src/main/java/org/ginwithouta/leetcode/Problem_0150_EvaluateReversePolishNotation.java
@@ -0,0 +1,38 @@
+package org.ginwithouta.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/1/21
+ * Problem 逆波兰表达式求职
+ */
+public class Problem_0150_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if ("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token)) {
+                int right = stack.pop(), left = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(left + right);
+                        break;
+                    case "-":
+                        stack.push(left - right);
+                        break;
+                    case "*":
+                        stack.push(left * right);
+                        break;
+                    case "/":
+                        stack.push(left / right);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+        return stack.peek();
+    }
+}
