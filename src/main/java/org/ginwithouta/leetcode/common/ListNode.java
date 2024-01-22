4c952b99c96c865cd5400e81ca9480d73a7e5003
ginwithouta <ginwithouta@gmail.com>
2024-01-22 18:16:34 +0800

    【FEATURE】解决 LeetCode 0138 题，随机链表的复制

--- src/main/java/org/ginwithouta/leetcode/common/ListNode.java
+++ src/main/java/org/ginwithouta/leetcode/common/ListNode.java
@@ -6,6 +6,7 @@ package org.ginwithouta.leetcode.common;
 public class ListNode {
     public int val;
     public ListNode next;
+    public ListNode random;
     public ListNode() {}
     public ListNode(int val) { this.val = val; this.next = null; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
