b03334c177699ba62bde706fdec33b71a527b612
ginwithouta <ginwithouta@gmail.com>
2024-01-24 11:12:41 +0800

    【FEATURE】解决 LeetCode 0104 题，二叉树的最大深度

---
+++ src/main/java/org/ginwithouta/leetcode/Problem_0104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package org.ginwithouta.leetcode;
+
+import org.ginwithouta.leetcode.common.TreeNode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/1/24
+ * Problem 二叉树的最大深度
+ */
+public class Problem_0104_MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        /*
+         * 一个递归解决
+         */
+        if (root == null) {
+            return 0;
+        } if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
