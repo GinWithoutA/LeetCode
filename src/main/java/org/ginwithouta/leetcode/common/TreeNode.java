5a4d8bc8292bdae0482c3f9cefa6c7448df30619
ginwithouta <ginwithouta@gmail.com>
2024-01-24 22:26:52 +0800

    【FIX BUG】优化二叉树结点共公共类的构造函数

--- src/main/java/org/ginwithouta/leetcode/common/TreeNode.java
+++ src/main/java/org/ginwithouta/leetcode/common/TreeNode.java
@@ -1,14 +1,23 @@
 package org.ginwithouta.leetcode.common;
 
-import lombok.RequiredArgsConstructor;
 
 /**
  * 二叉树节的点类
  */
-@RequiredArgsConstructor
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
     public TreeNode next;
+    public TreeNode() {}
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
