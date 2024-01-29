acf27ab2f9001f0f41394b971578a72d4069be9d
ginwithouta <ginwithouta@gmail.com>
2024-01-29 12:03:35 +0800

    【FEATURE】新增图的公共节点类 Node

---
+++ src/main/java/org/ginwithouta/leetcode/common/graph/Node.java
@@ -0,0 +1,26 @@
+package org.ginwithouta.leetcode.common.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/1/29
+ * 图的公共节点类
+ */
+public class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        this.val = 0;
+        this.neighbors = new ArrayList<>();
+    }
+    public Node(int _val) {
+        this.val = _val;
+        this.neighbors = new ArrayList<>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        this.val = _val;
+        this.neighbors = _neighbors;
+    }
+}
