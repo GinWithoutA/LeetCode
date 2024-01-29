3ba1036d8728be3d93fbb79984cfb966352d6c93
ginwithouta <ginwithouta@gmail.com>
2024-01-29 12:48:47 +0800

    【FIX BUG】有向图相关属性构造函数修改

--- src/main/java/org/ginwithouta/leetcode/common/graph/Node.java
+++ src/main/java/org/ginwithouta/leetcode/common/graph/Node.java
@@ -14,11 +14,15 @@ public class Node {
     public int in;
     public List<Node> nexts;
     public Node() {
+        this.in = 0;
         this.val = 0;
+        this.nexts = new ArrayList<>();
         this.neighbors = new ArrayList<>();
     }
     public Node(int _val) {
+        this.in = 0;
         this.val = _val;
+        this.nexts = new ArrayList<>();
         this.neighbors = new ArrayList<>();
     }
     public Node(int _val, ArrayList<Node> _neighbors) {
