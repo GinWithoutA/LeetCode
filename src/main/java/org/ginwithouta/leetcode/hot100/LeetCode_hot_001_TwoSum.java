14ea11ddbe86e9d4d66268abf509ec225fcd0fc1
ginwithouta <ginwithouta@gmail.com>
2024-02-16 15:39:45 +0800

    【FEATURE】新增 LeetCode 热题100，001题，两数之和

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_hot_001_TwoSum.java
@@ -0,0 +1,21 @@
+package org.ginwithouta.leetcode.hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/16
+ */
+public class LeetCode_hot_001_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                return new int[] {map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+}
