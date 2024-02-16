d64f1b45eef10e3109e3b0c1e6105e6883776c75
ginwithouta <ginwithouta@gmail.com>
2024-02-16 16:20:59 +0800

    【FEATURE】新增 LeetCode 热题100，001题，字母异位词分组

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0049_GroupAnagrams.java
@@ -0,0 +1,24 @@
+package org.ginwithouta.leetcode.hot100;
+
+import java.util.*;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/16
+ * LeetCode 热题 100 字母异位词分组
+ */
+public class LeetCode_Hot_0049_GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
