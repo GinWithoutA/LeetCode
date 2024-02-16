6c21cc9e45348cf1fe464e8a32046b67a8d34fb7
ginwithouta <ginwithouta@gmail.com>
2024-02-16 16:37:25 +0800

    【FEATURE】解决 LeetCode Hot 第三题，最长连续序列

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0128_LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+package org.ginwithouta.leetcode.hot100;
+
+import java.util.Arrays;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/16
+ * LeetCode Hot 100 最长连续序列
+ */
+public class LeetCode_Hot_0128_LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int right = 1, ans = 0, len = 1, prevNum = nums[0];
+        while (right < nums.length) {
+            if (nums[right] == prevNum) {
+                ++right;
+            } else if (nums[right] == prevNum + 1) {
+                prevNum = nums[right++];
+                ++len;
+            } else {
+                ans = Math.max(ans, len);
+                prevNum = nums[right++];
+                len = 1;
+            }
+        }
+        return Math.max(ans, len);
+    }
+}
