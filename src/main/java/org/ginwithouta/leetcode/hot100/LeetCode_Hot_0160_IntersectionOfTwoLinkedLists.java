6f8ed7859a36910ee91fa2818f8e7a760ebbb8a9
ginwithouta <ginwithouta@gmail.com>
2024-02-20 10:55:46 +0800

    【FEATURE】解决 LeetCode Hot 第二十二题，相交链表

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,38 @@
+package org.ginwithouta.leetcode.hot100;
+
+import org.ginwithouta.leetcode.common.ListNode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/20
+ * LeetCode Hot 100 相交链表
+ */
+public class LeetCode_Hot_0160_IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode top = headA, bot = headB;
+        int topCount = 1, botCount = 1;
+        while (top.next != null) {
+            top = top.next;
+            ++topCount;
+        }
+        while (bot.next != null) {
+            bot = bot.next;
+            ++botCount;
+        }
+        if (top != bot) {
+            return null;
+        }
+        int big = Math.max(topCount, botCount), small = Math.min(topCount, botCount);
+        top = big == topCount ? headA : headB;
+        bot = top == headA ? headB : headA;
+        while (big != small) {
+            top = top.next;
+            --big;
+        }
+        while (top != bot) {
+            top = top.next;
+            bot = bot.next;
+        }
+        return top;
+    }
+}
