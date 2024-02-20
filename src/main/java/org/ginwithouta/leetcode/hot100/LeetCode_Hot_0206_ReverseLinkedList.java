92dcace96b03e4700ad6d7fd5cd8a5efff2b1c2a
ginwithouta <ginwithouta@gmail.com>
2024-02-20 19:08:09 +0800

    【FEATURE】解决 LeetCode Hot 第二十三题，反转链表

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0206_ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package org.ginwithouta.leetcode.hot100;
+
+import org.ginwithouta.leetcode.common.ListNode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/20
+ * LeetCode Hot 100 反转链表
+ */
+public class LeetCode_Hot_0206_ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null, next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
