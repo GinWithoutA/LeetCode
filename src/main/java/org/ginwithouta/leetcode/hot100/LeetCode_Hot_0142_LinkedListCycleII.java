cc8d923f1bf44e995dfd6b3b7d7d15ea8470ce63
ginwithouta <ginwithouta@gmail.com>
2024-02-21 00:22:40 +0800

    【FEATURE】解决 LeetCode Hot 第二十六题，环形链表Ⅱ

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0142_LinkedListCycleII.java
@@ -0,0 +1,30 @@
+package org.ginwithouta.leetcode.hot100;
+
+import org.ginwithouta.leetcode.common.ListNode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/20
+ * LeetCode Hot 100 环形链表Ⅱ
+ */
+public class LeetCode_Hot_0142_LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        do {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (fast != slow);
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
