f33b86eb00d9a850791aade248e20b7450119c25
ginwithouta <ginwithouta@gmail.com>
2024-02-21 22:21:32 +0800

    【FEATURE】解决 LeetCode Hot 第三十八题，反转二叉树

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0266_InvertBinaryTree.java
@@ -0,0 +1,22 @@
+package org.ginwithouta.leetcode.hot100;
+
+import org.ginwithouta.leetcode.common.tree.TreeNode;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/21
+ * LeetCode Hot 100 反转二叉树
+ */
+public class LeetCode_Hot_0266_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
