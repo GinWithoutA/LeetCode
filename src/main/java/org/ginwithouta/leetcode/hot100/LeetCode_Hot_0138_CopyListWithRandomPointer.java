537c081561264255cc670cfebffff3cf64a67fc1
ginwithouta <ginwithouta@gmail.com>
2024-02-21 12:07:55 +0800

    【FEATURE】解决 LeetCode Hot 第三十二题，随机链表的复制

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_0138_CopyListWithRandomPointer.java
@@ -0,0 +1,47 @@
+package org.ginwithouta.leetcode.hot100;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/21
+ */
+public class LeetCode_Hot_0138_CopyListWithRandomPointer {
+    public static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node dummy = new Node(-1), pre, cur = head;
+        while (cur != null) {
+            Node node = new Node(cur.val);
+            node.next = cur.next;
+            cur.next = node;
+            cur = node.next;
+        }
+        pre = head;
+        while (pre != null) {
+            cur = pre.next;
+            cur.random = pre.random == null ? null : pre.random.next;
+            pre = cur.next;
+        }
+        cur = head;
+        pre = dummy;
+        while (cur != null) {
+            pre.next = cur.next;
+            cur.next = cur.next.next;
+            cur = cur.next;
+            pre = pre.next;
+        }
+        return dummy.next;
+    }
+}
