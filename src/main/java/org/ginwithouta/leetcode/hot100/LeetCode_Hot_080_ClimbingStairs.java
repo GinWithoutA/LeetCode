128b3ae28a45ff87a47ee18dc23de6aadbdba025
ginwithouta <ginwithouta@gmail.com>
2024-02-29 15:43:28 +0800

    【FEATURE】解决 LeetCode Hot 第八十题，爬楼梯

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_080_ClimbingStairs.java
@@ -0,0 +1,27 @@
+package org.ginwithouta.leetcode.hot100;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/2/29
+ * LeetCode Hot 100 爬楼梯
+ */
+public class LeetCode_Hot_080_ClimbingStairs {
+    public int climbStairs(int n) {
+        /*
+         * 本题动态规划的公式是 f(x) = f(x - 1) + f(x - 2)
+         */
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < dp.length; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[dp.length - 1];
+    }
+}
