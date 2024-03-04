9c20f3992a14796ba43c18b2466cb58dd9e8bbcf
ginwithouta <ginwithouta@gmail.com>
2024-03-04 10:33:38 +0800

    【FEATURE】解决 LeetCode Hot 第八十三题，零钱兑换

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_083_CoinChange.java
@@ -0,0 +1,27 @@
+package org.ginwithouta.leetcode.hot100;
+
+import java.util.Arrays;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/3/4
+ * LeetCode Hot 100 零钱兑换
+ */
+public class LeetCode_Hot_083_CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+
+
+}
