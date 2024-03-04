64a94518982c25d3e608e877819125ab81517dfb
ginwithouta <ginwithouta@gmail.com>
2024-03-04 10:04:38 +0800

    【FEATURE】解决 LeetCode Hot 第八十二题，完全平方数

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_082_PerfectSquares.java
@@ -0,0 +1,24 @@
+package org.ginwithouta.leetcode.hot100;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/3/4
+ * LeetCode Hot 100 完全平方数
+ */
+public class LeetCode_Hot_082_PerfectSquares {
+    public int numSquares(int n) {
+        /*
+         * 计算 i - j * j 需要多少个数表示，最后计算每一个 dp 位置的时候
+         * 都需要加上当前的 1 个用来表示的数
+         */
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            int cnt = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                cnt = Math.min(cnt, dp[i - j * j]);
+            }
+            dp[i] = 1 + cnt;
+        }
+        return dp[n];
+    }
+}
