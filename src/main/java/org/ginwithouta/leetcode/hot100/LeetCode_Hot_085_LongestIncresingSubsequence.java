4a5dbde15b490a6ad95cb0850d665b508d2915cd
ginwithouta <ginwithouta@gmail.com>
2024-03-04 11:36:27 +0800

    【FEATURE】解决 LeetCode Hot 第八十五题，最长递增子序列

---
+++ src/main/java/org/ginwithouta/leetcode/hot100/LeetCode_Hot_085_LongestIncresingSubsequence.java
@@ -0,0 +1,29 @@
+package org.ginwithouta.leetcode.hot100;
+
+import java.util.Arrays;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/3/4
+ * LeetCode Hot 100 最长递增子序列
+ */
+public class LeetCode_Hot_085_LongestIncresingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+
+        if (nums.length == 1) {
+            return 1;
+        }
+        int[] dp = new int[nums.length];
+        int max = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < nums.length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
