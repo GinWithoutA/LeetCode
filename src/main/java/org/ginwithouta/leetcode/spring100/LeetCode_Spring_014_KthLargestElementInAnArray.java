8d6736c608c528daac5b7a7ee402f23aeb358353
ginwithouta <ginwithouta@gmail.com>
2024-03-25 11:07:33 +0800

    【FEATURE】解决 LeetCode 春招 100 数组中的第K个最大元素

---
+++ src/main/java/org/ginwithouta/leetcode/spring100/LeetCode_Spring_014_KthLargestElementInAnArray.java
@@ -0,0 +1,46 @@
+package org.ginwithouta.leetcode.spring100;
+
+import javax.swing.text.html.parser.TagElement;
+
+/**
+ * @author Ginwithouta
+ * Generate at 2024/3/25
+ * LeetCode 春招 100 数组中的第 K 个最大元素
+ */
+public class LeetCode_Spring_014_KthLargestElementInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        int left = 0, right = nums.length - 1, target = nums.length - k;
+        while (left < right) {
+            int pivot = nums[left + (int) (Math.random() * (right - left))];
+            int[] range = partition(nums, left, right, pivot);
+            if (target >= range[0] && target <= range[1]) {
+                return nums[target];
+            } else if (target < range[0]) {
+                right = range[0] - 1;
+            } else {
+                left = range[1] + 1;
+            }
+        }
+        return nums[left];
+    }
+
+    private int[] partition(int[] nums, int left, int right, int pivot) {
+        int less = left - 1, more = right, idx = left;
+        while (idx <= more) {
+            if (nums[idx] < pivot) {
+                swap(nums, ++less, idx++);
+            } else if (nums[idx] > pivot) {
+                swap(nums, idx, more--);
+            } else {
+                ++idx;
+            }
+        }
+        return new int[] { less + 1, more };
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
